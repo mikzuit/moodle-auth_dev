374cc3c00b9bd39bbcd18aab3f3d3559ef13bba8
Carlos <carlos.escobedo@gmail.com>
2014-05-20 23:03:52 +0200

    Init repository

---
+++ README.txt
@@ -0,0 +1,38 @@
+Moodle Auth Development
+===============
+
+Information
+-----------
+
+This plugin allow a user not logout when this user has loginas. 
+It's useful to developers and main administrators.
+
+
+Version
+-------
+Moodle 2.7+ and 2.6+.
+
+TO INSTALL:
+
+Git way
+- To install it using git, type this command in the root of your Moodle install:
+```
+git clone git@github.com:cescobedo/moodle-auth_dev.git auth/dev
+```
+
+
+Download way
+- Download the zip from <https://github.com/cescobedo/moodle-auth_dev/archive/master.zip>
+- Unzip it into  auth/ folder in your Moodle,
+- Rename the new folder "moodle-auth_dev" to "dev"
+- Enjoy!!!
+
+FUTURE SCOPE:
+- ADD useful actions to developer like not create user in ldap_plugin when user not exist.
+- Redirect to specific URL when user logout.
+
+Author
+------
+Carlos Escobedo
+- <http://www.twitter.com/carlosagile>
+- <https://coderwall.com/carlosagile>
---
+++ auth.php
@@ -0,0 +1,68 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Authentication Development Tools
+ * Uses to developers and main admins.
+ *
+ * @package    auth_dev
+ * @author     Carlos Escobedo <http://www.twitter.com/carlosagile>)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->libdir.'/authlib.php');
+
+/**
+ * Authentication Development Tools plugin.
+ *
+ * @package    auth
+ * @subpackage dev
+ * @author     Carlos Escobedo <http://www.twitter.com/carlosagile>)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class auth_plugin_dev extends auth_plugin_base {
+    /**
+    * The name of the component. Used by the configuration.
+    */
+    const COMPONENT_NAME = 'auth_dev';
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        $this->authtype = 'dev';
+        $this->config = get_config(self::COMPONENT_NAME);
+    }
+
+    public function prelogout_hook() {
+        global $CFG, $SESSION;
+
+        if (\core\session\manager::is_loggedinas()) {
+            $id = optional_param('id', 0, PARAM_INT);
+            // IF ID==0 then logout request, notthing to do.
+            if ($id) {
+                $realuser = \core\session\manager::get_realuser();
+                complete_user_login($realuser);
+                $SESSION->wantsurl = "$CFG->wwwroot/course/view.php?id=".$id;
+                redirect($SESSION->wantsurl);
+            }
+        }
+    }
+}
+
+
