616b9f0b8f5923207b99a99f555f61533a6346f3
Carlos <carlos.escobedo@gmail.com>
2015-10-05 10:47:33 +0200

    Update version.php
    Fix require version to moodle 2.6

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2014051300;
-$plugin->requires  = 2013111800;
+$plugin->requires  = 2013101800;
 $plugin->component = 'auth_dev';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '2.0';
